79d7a011bea57d03270ca5d434e1078062f24a21
Jerson Pérez <ing.jersonperez@gmail.com>
2021-08-13 22:52:00 +0000

    [Fix] doxument style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file named Holberton
 
 file{'holberton':
-    path     => '/tmp/holberton',
-    content  => 'I love Puppet',
-    mode     => '0744',
-    owner    => 'www-data',
-    group    => 'www-data'
+    path    => '/tmp/holberton',
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data'
 }
