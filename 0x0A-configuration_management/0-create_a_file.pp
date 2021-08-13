c4b543dee538a472abe120014b56af97c80b9947
Jerson Pérez <ing.jersonperez@gmail.com>
2021-08-13 22:48:36 +0000

    [Update] remove variable and insetr the content directly

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # creates a file named Holberton
 
-$cont = 'I love Puppet'
-
 file{'holberton':
     path     => '/tmp/holberton',
-    content  => $cont,
+    content  => 'I love Puppet',
     mode     => '0744',
     owner    => 'www-data',
     group    => 'www-data'
