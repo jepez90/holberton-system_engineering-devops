3bd31bfe1a4875bb2d5f1648602b5ab329c1253e
Jerson Pérez <ing.jersonperez@gmail.com>
2021-08-18 19:09:43 +0000

    [Fix] error with syntax

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # connect with the server using puppet
 
-exec { "connect":
-    command  => 'echo "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/ssh_config':
-    path     => '/bin/'
+exec { 'connect':
+    command  => 'echo "IdentityFile ~/.ssh/holberton\nPasswordAuthentication no" >> /etc/ssh/ssh_config',
+    path     => '/bin/',
     provider => 'shell'
 }
