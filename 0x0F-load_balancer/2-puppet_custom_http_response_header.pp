4cd094d0ceac5d8e88205520623f87e2622a2d72
Jerson Pérez <ing.jersonperez@gmail.com>
2021-08-24 02:51:35 +0000

    [Fix] code style

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,16 @@
 # Install nginx server and configure to add a custom header to response
 
-exec  {'configure nginx':
-    command  => 'apt-get -y update',
-    command  => 'apt-get -y install nginx',
-    command  => 'sed -i "s/server_name _;/server_name _;\n  add_header X-Served-By $hostname always;\n/" /etc/nginx/sites-available/default',
+exec  {'install_nginx':
+    command  => 'apt-get -y update; apt-get -y install nginx',
+    provider => 'shell'
+}
+
+exec  {'configure_nginx':
+    command  => 'sed -i "s/server_name _;/server_name _;\n add_header X-Served-By $hostname always;\n/" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+exec  {'restart_nginx':
     command  => 'sudo service nginx restart',
     provider => 'shell'
 }
